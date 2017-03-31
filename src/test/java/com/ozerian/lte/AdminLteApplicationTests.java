4b25566b7a32201aab1563adba8bd37cf0f31093
ivan_ozerian <ivan.ozeryan@gmail.com>
2017-03-31 19:12:32 +0300

    Initial commit.

---
+++ src/test/java/com/ozerian/lte/AdminLteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ozerian.lte;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminLteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
