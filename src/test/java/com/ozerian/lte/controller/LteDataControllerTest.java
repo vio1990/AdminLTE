008306a5219e9f6b12c467c95d7edebd12af0b46
ivan_ozerian <ivan.ozeryan@gmail.com>
2017-03-31 20:46:55 +0300

    Added test cases.

--- src/test/java/com/ozerian/lte/controller/LteDataControllerTest.java
+++ src/test/java/com/ozerian/lte/controller/LteDataControllerTest.java
@@ -17,11 +17,17 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import javax.inject.Inject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,6 +46,42 @@ public class LteDataControllerTest {
 
     @Test
     public void getAllLteData() throws Exception {
+        LteData firstLteData = new LteData(6L, "Webkit", "Safari 1.2",
+                "S60", "4.3", "A");
+        LteData secondLteData = new LteData(7L, "Gecko", "FireFox 1.0",
+                "iPod", "1.7", "A");
+        List<LteData> allLteDataList = new ArrayList<>();
+        allLteDataList.add(firstLteData);
+        allLteDataList.add(secondLteData);
+
+        when(lteDataService.getAllLteData()).thenReturn(allLteDataList);
+
+        MvcResult result = mockMvc.perform(get("/tables/data")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String expectedResult = new StringBuilder()
+                .append("[{")
+                .append("\"id\":6,")
+                .append("\"renderingEngine\":\"Webkit\",")
+                .append("\"browser\":\"Safari 1.2\",")
+                .append("\"platform\":\"S60\",")
+                .append("\"engineVersion\":\"4.3\",")
+                .append("\"cssGrade\":\"A\"")
+                .append("},{")
+                .append("\"id\":7,")
+                .append("\"renderingEngine\":\"Gecko\",")
+                .append("\"browser\":\"FireFox 1.0\",")
+                .append("\"platform\":\"iPod\",")
+                .append("\"engineVersion\":\"1.7\",")
+                .append("\"cssGrade\":\"A\"")
+                .append("}]")
+                .toString();
+
+        String actualResult = result.getResponse().getContentAsString();
+        assertNotNull(result.getResponse().getContentAsString());
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
@@ -69,11 +111,15 @@ public class LteDataControllerTest {
         String actualResult = result.getResponse().getContentAsString();
         assertNotNull(result.getResponse().getContentAsString());
         assertEquals(expectedResult, actualResult);
-
     }
 
     @Test
     public void removeLteDataById() throws Exception {
+        mockMvc.perform(delete("/tables/data/2")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(lteDataService, times(1)).deleteLteDataById(2L);
     }
 
     @Test
@@ -87,6 +133,42 @@ public class LteDataControllerTest {
 
     @Test
     public void getOrderedByRenEngineLteData() throws Exception {
+        LteData firstLteData = new LteData(8L, "Trident", "Safari 1.2",
+                "OSX.3", "4.3", "A");
+        LteData secondLteData = new LteData(9L, "Gecko", "FireFox 1.0",
+                "iPod", "1.7", "A");
+        List<LteData> allLteDataList = new ArrayList<>();
+        allLteDataList.add(firstLteData);
+        allLteDataList.add(secondLteData);
+
+        when(lteDataService.getAllLteDataGroupedByRenderingName()).thenReturn(allLteDataList);
+
+        MvcResult result = mockMvc.perform(get("/tables/data/ordered")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String expectedResult = new StringBuilder()
+                .append("[{")
+                .append("\"id\":8,")
+                .append("\"renderingEngine\":\"Trident\",")
+                .append("\"browser\":\"Safari 1.2\",")
+                .append("\"platform\":\"OSX.3\",")
+                .append("\"engineVersion\":\"4.3\",")
+                .append("\"cssGrade\":\"A\"")
+                .append("},{")
+                .append("\"id\":9,")
+                .append("\"renderingEngine\":\"Gecko\",")
+                .append("\"browser\":\"FireFox 1.0\",")
+                .append("\"platform\":\"iPod\",")
+                .append("\"engineVersion\":\"1.7\",")
+                .append("\"cssGrade\":\"A\"")
+                .append("}]")
+                .toString();
+
+        String actualResult = result.getResponse().getContentAsString();
+        assertNotNull(result.getResponse().getContentAsString());
+        assertEquals(expectedResult, actualResult);
     }
 
 }
