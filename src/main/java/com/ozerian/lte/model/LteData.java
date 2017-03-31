049cf757ae4f1f93da6411b4820f93b09a4800bc
ivan_ozerian <ivan.ozeryan@gmail.com>
2017-04-01 00:34:02 +0300

    Refactoring.

--- src/main/java/com/ozerian/lte/model/LteData.java
+++ src/main/java/com/ozerian/lte/model/LteData.java
@@ -1,8 +1,16 @@
 package com.ozerian.lte.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Entity class for representation of necessary data.
