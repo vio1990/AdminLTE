e1d157aa00f7b93e61d5eac986fd9b62b21e2760
ivan_ozerian <ivan.ozeryan@gmail.com>
2017-03-31 19:13:42 +0300

    Created LteData entity.

---
+++ src/main/java/com/ozerian/lte/model/LteData.java
@@ -0,0 +1,34 @@
+package com.ozerian.lte.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+/**
+ * Entity class for representation of necessary data.
+ */
+@Entity
+@Table(name = "LteData")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class LteData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String renderingEngine;
+
+    private String browser;
+
+    private String platform;
+
+    private String engineVersion;
+
+    private String cssGrade;
+
+}
