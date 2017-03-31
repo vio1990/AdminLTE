c3ea2ff56adce31bd67353eecfa844b8503a62f7
ivan_ozerian <ivan.ozeryan@gmail.com>
2017-03-31 19:13:21 +0300

    Initial commit.

---
+++ src/main/java/com/ozerian/lte/AdminLteApplication.java
@@ -0,0 +1,12 @@
+package com.ozerian.lte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminLteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminLteApplication.class, args);
+	}
+}
