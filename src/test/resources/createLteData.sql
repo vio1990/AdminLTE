08b0b18c971b20ed720d4eb3ec418d6f8be21cf9
ivan_ozerian <ivan.ozeryan@gmail.com>
2017-03-31 23:26:50 +0300

    Created sql script for creation of LteData.

---
+++ src/test/resources/createLteData.sql
@@ -0,0 +1,3 @@
+INSERT INTO LteData VALUES (1, 'Webkit', 'FireFox 3.1', 'iPod', '3.1', 'A');
+INSERT INTO LteData VALUES (2, 'Gecko', 'Chrome 3.1', '60S', '3.2', 'A');
+INSERT INTO LteData VALUES (3, 'Trident', 'Safari 2.0', 'OSX.3', '3.2', 'A');
